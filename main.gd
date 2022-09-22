104e6a2c262daf46c2f79c7ed57d6bb689ece613
Jummit <jummit@web.de>
2022-09-22 18:21:28 +0200

    Update demo to 4.0

--- main.gd
+++ main.gd
@@ -1,11 +1,10 @@
 extends Control
 
-const PropertyPanel = preload("res://addons/property_panel/property_panel.gd")
 const Properties = preload("res://addons/property_panel/properties.gd")
 
-onready var middle_property_panel : PropertyPanel = $MiddlePropertyPanel
-onready var top_property_panel : PropertyPanel = $TopPropertyPanel
-onready var editable_texture : TextureRect = $EditableTexture
+@onready var middle_property_panel : PropertyPanel = $MiddlePropertyPanel
+@onready var top_property_panel : PropertyPanel = $TopPropertyPanel
+@onready var editable_texture : TextureRect = $EditableTexture
 
 func _ready() -> void:
 	top_property_panel.set_properties([
@@ -17,7 +16,7 @@ func _ready() -> void:
 	middle_property_panel.set_properties([
 		Properties.StringProperty.new("name", editable_texture.name),
 		Properties.BoolProperty.new("visible", true),
-		Properties.FloatProperty.new("rect_rotation", 1, 360),
+		Properties.FloatProperty.new("rotation", 1, 360),
 		Properties.ColorProperty.new("modulate"),
 		Properties.BoolProperty.new("flip_v"),
 	])
